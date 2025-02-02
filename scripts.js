99e2744fde29a91c28f80d881649e37ca7e23a4c
sharktide <meherrihaan@gmail.com>
2025-02-02 09:14:46 -0500

    Test of vercel.json

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const recyclebot = document.getElementsByClassName("project01")
+async function recyclebotclick() {
+    recyclebot.onclick() = function() {
+        window.location.href("/recyclebot")
+    }
+}
