173952401b4fbbd942b9e136604252726918abf5
sharktide <meherrihaan@gmail.com>
2025-02-02 11:07:30 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const recyclebot = document.getElementsByClassName("project01")
+const recyclebot = document.getElementsByClassName(".project01")
 async function recyclebotclick() {
     recyclebot.onclick() = function() {
         window.location.href("/recyclebot")
