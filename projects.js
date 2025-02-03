94288ffca856158ed600810f3f9e0396c1b8cf6b
sharktide <meherrihaan@gmail.com>
2025-02-03 16:53:03 -0500

    added styling and basic js

---
+++ projects.js
@@ -0,0 +1,8 @@
+console.log("program started")
+document.getElementById("01").onclick = function() {
+    console.log("01")
+}
+
+document.getElementById("02").onclick = function() {
+    console.log("02")
+}
