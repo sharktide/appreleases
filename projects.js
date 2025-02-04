9fcf5c803d65e25e25957da28d26f91dbae1773f
sharktide <meherrihaan@gmail.com>
2025-02-04 12:18:43 -0500

    /

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 console.log("program started")
 document.getElementById("01").onclick = function() {
     console.log("01")
+    window.location.href = "/recyclebot"
 }
 
 document.getElementById("02").onclick = function() {
--- scripts.js
+++
@@ -1,6 +0,0 @@
-const recyclebot = document.getElementsByClassName(".project01")
-async function recyclebotclick() {
-    recyclebot.onclick() = function() {
-        window.location.href("/recyclebot")
-    }
-}
