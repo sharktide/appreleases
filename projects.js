fbeb67e553fb737c4827d6045a9b6bb292190e90
sharktide <meherrihaan@gmail.com>
2025-02-06 18:55:39 -0500

    Revamp project page

--- projects.js
+++ projects.js
@@ -1,9 +1,26 @@
-console.log("program started")
-document.getElementById("01").onclick = function() {
-    console.log("01")
-    window.location.href = "/recyclebot"
-}
+document.getElementById('search-bar-repos').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    const githubRepos = document.querySelectorAll('.github-repositories a');
 
-document.getElementById("02").onclick = function() {
-    console.log("02")
-}
+    githubRepos.forEach(repo => {
+        const repoName = repo.querySelector('img').id.toLowerCase();
+        if (repoName.includes(query)) {
+            repo.style.display = 'block';
+        } else {
+            repo.style.display = 'none';
+        }
+    });
+});
+
+document.getElementById('search-bar-models').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    const huggingfaceModels = document.querySelectorAll('.huggingface-models');
+
+    huggingfaceModels.forEach(model => {
+        if (model.textContent.toLowerCase().includes(query)) {
+            model.style.display = 'block';
+        } else {
+            model.style.display = 'none';
+        }
+    });
+});
